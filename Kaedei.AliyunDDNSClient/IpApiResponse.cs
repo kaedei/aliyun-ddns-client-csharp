89ed84b26cf056448bc8fb6d0e437cc62955dd7d
kaedei <kaedei@live.cn>
2016-03-21 11:50:18 +0800

    Basic impl

---
+++ Kaedei.AliyunDDNSClient/IpApiResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Kaedei.AliyunDDNSClient
+{
+	public class IpApiResponse
+	{
+		public string @as { get; set; }
+		public string city { get; set; }
+		public string country { get; set; }
+		public string countryCode { get; set; }
+		public string isp { get; set; }
+		public double lat { get; set; }
+		public double lon { get; set; }
+		public string org { get; set; }
+
+		/// <summary>
+		/// 当前公网IP地址
+		/// </summary>
+		public string query { get; set; }
+
+		public string region { get; set; }
+		public string regionName { get; set; }
+		public string status { get; set; }
+		public string timezone { get; set; }
+		public string zip { get; set; }
+	}
+}
---
+++ Kaedei.AliyunDDNSClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Aliyun.Api;
+using Aliyun.Api.DNS.DNS20150109.Request;
+using Newtonsoft.Json;
+
+namespace Kaedei.AliyunDDNSClient
+{
+	class Program
+	{
+		private static void Main(string[] args)
+		{
+			var configs = File.ReadAllLines("config.txt");
+			var accessKeyId = configs[0].Trim(); //Access Key ID，如 DR2DPjKmg4ww0e79
+			var accessKeySecret = configs[1].Trim(); //Access Key Secret，如 ysHnd1dhWvoOmbdWKx04evlVEdXEW7 
+			var domainName = configs[2].Trim(); //域名，如 google.com
+			var rr = configs[3].Trim(); //子域名，如 www
+
+			var aliyunClient = new DefaultAliyunClient("http://dns.aliyuncs.com/", accessKeyId, accessKeySecret);
+			var req = new DescribeDomainRecordsRequest() {DomainName = domainName};
+			var response = aliyunClient.Execute(req);
+
+			var updateRecord = response.DomainRecords.FirstOrDefault(rec => rec.RR == rr && rec.Type == "A");
+			if (updateRecord == null)
+				return;
+
+			//获取IP
+			var ipJson = new HttpClient().GetStringAsync("http://ip-api.com/json").Result;
+			var ip = JsonConvert.DeserializeObject<IpApiResponse>(ipJson).query;
+
+			if (updateRecord.Value != ip)
+			{
+				var changeValueRequest = new UpdateDomainRecordRequest()
+				{
+					RecordId = updateRecord.RecordId,
+					Value = ip
+				};
+				aliyunClient.Execute(changeValueRequest);
+				Console.WriteLine("Update finished.");
+			}
+		}
+	}
+}
