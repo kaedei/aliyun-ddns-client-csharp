05b8699692043b0e20f9d8756c692de54b6ae665
kaedei <kaedei@live.cn>
2016-03-23 00:55:24 +0800

    catch exceptions

--- Kaedei.AliyunDDNSClient/Program.cs
+++ Kaedei.AliyunDDNSClient/Program.cs
@@ -13,40 +13,48 @@ namespace Kaedei.AliyunDDNSClient
 	{
 		private static void Main(string[] args)
 		{
-			var configs = File.ReadAllLines("config.txt");
-			var accessKeyId = configs[0].Trim(); //Access Key ID，如 DR2DPjKmg4ww0e79
-			var accessKeySecret = configs[1].Trim(); //Access Key Secret，如 ysHnd1dhWvoOmbdWKx04evlVEdXEW7 
-			var domainName = configs[2].Trim(); //域名，如 google.com
-			var rr = configs[3].Trim(); //子域名，如 www
-			Console.WriteLine("Updating {0} of domain {1}", rr, domainName);
-
-			var aliyunClient = new DefaultAliyunClient("http://dns.aliyuncs.com/", accessKeyId, accessKeySecret);
-			var req = new DescribeDomainRecordsRequest() {DomainName = domainName};
-			var response = aliyunClient.Execute(req);
-
-			var updateRecord = response.DomainRecords.FirstOrDefault(rec => rec.RR == rr && rec.Type == "A");
-			if (updateRecord == null)
-				return;
-			Console.WriteLine("Domain record IP is " + updateRecord.Value);
-
-			//获取IP
-			var ipJson = new HttpClient().GetStringAsync("http://ip-api.com/json").Result;
-			var ip = JsonConvert.DeserializeObject<IpApiResponse>(ipJson).query;
-			Console.WriteLine("Current IP is " + ip);
-
-			if (updateRecord.Value != ip)
+			try
 			{
-				var changeValueRequest = new UpdateDomainRecordRequest()
+
+				var configs = File.ReadAllLines("config.txt");
+				var accessKeyId = configs[0].Trim(); //Access Key ID，如 DR2DPjKmg4ww0e79
+				var accessKeySecret = configs[1].Trim(); //Access Key Secret，如 ysHnd1dhWvoOmbdWKx04evlVEdXEW7 
+				var domainName = configs[2].Trim(); //域名，如 google.com
+				var rr = configs[3].Trim(); //子域名，如 www
+				Console.WriteLine("Updating {0} of domain {1}", rr, domainName);
+
+				var aliyunClient = new DefaultAliyunClient("http://dns.aliyuncs.com/", accessKeyId, accessKeySecret);
+				var req = new DescribeDomainRecordsRequest() { DomainName = domainName };
+				var response = aliyunClient.Execute(req);
+
+				var updateRecord = response.DomainRecords.FirstOrDefault(rec => rec.RR == rr && rec.Type == "A");
+				if (updateRecord == null)
+					return;
+				Console.WriteLine("Domain record IP is " + updateRecord.Value);
+
+				//获取IP
+				var ipJson = new HttpClient().GetStringAsync("http://ip-api.com/json").Result;
+				var ip = JsonConvert.DeserializeObject<IpApiResponse>(ipJson).query;
+				Console.WriteLine("Current IP is " + ip);
+
+				if (updateRecord.Value != ip)
 				{
-					RecordId = updateRecord.RecordId,
-					Value = ip
-				};
-				aliyunClient.Execute(changeValueRequest);
-				Console.WriteLine("Update finished.");
+					var changeValueRequest = new UpdateDomainRecordRequest()
+					{
+						RecordId = updateRecord.RecordId,
+						Value = ip
+					};
+					aliyunClient.Execute(changeValueRequest);
+					Console.WriteLine("Update finished.");
+				}
+				else
+				{
+					Console.WriteLine("IPs are same now. Exiting");
+				}
 			}
-			else
+			catch (Exception ex)
 			{
-				Console.WriteLine("IPs are same now. Exiting");
+				Console.WriteLine(ex.ToString());
 			}
 			Thread.Sleep(5000);
 		}
